4b20277db2e963a0001b89cc2407ea7b97983b43
Krishank696 <73972335+Krishank696@users.noreply.github.com>
2021-01-05 16:12:40 -0600

    Add files via upload

---
+++ bullets/p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,23 @@
+var bullet, speed, weight,
+var wall, thickness;
+
+thickness = random(22.83);
+wall=createSprite(1200, 200, thickness, height/2);
+speed = rand(200, 300);
+weight = rand(20, 50);
+
+
+function setup() {
+  createCanvas(1600,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(80, 80, 80);  
+  drawSprites();
+
+if(bullet.speed ===<250);
+bullet.shapeColor = ("red")
+
+else
+ bullet.shapeColor = ("green")
